4475307d7665e72e0ca2a3b58b96c16ea8525ef7
Leandro Machado <leandrovictoram@gmail.com>
2024-05-13 13:15:22 -0300

    initial project

---
+++ app/page.js
@@ -0,0 +1,17 @@
+"use client"
+
+import Link from "next/link";
+
+window.parent.postMessage('chatwoot-dashboard-app:fetch-info', '*')
+
+export default function Home() {
+  const getContext = () => {
+    console.log(window.parent.postMessage('chatwoot-dashboard-app:fetch-info', '*'))
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <button onClick={getContext} className="px-10 py-5 bg-white text-black">Enviar contexto</button >
+    </main>
+  );
+}
