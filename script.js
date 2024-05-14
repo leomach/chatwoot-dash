4c7253d1ed41287085ee8440ada522579a2c4f86
Leandro Machado <leandrovictoram@gmail.com>
2024-05-14 11:21:03 -0300

    build: postMessage

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function getContext () {
     window.addEventListener("message", function (event) {
-      
-        const eventData = event.data
-        console.log(eventData)
+      console.log(window.parent.postMessage('chatwoot-dashboard-app:fetch-info', '*'))
+      return window.parent.postMessage('chatwoot-dashboard-app:fetch-info', '*')
       }
     )
-
   }
+
   window.addEventListener("message", function (event) {  
       const eventData = event.data
       console.log(eventData)
