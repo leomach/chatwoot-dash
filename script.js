d64a57e7cda891277632308ccf5ab03aa99702c0
Leandro Machado <leandrovictoram@gmail.com>
2024-05-14 16:50:10 -0300

    feat: mutation complete

--- script.js
+++ script.js
@@ -14,15 +14,51 @@ let descricao = ''
 
 const token = 'eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJQaXBlZnkiLCJpYXQiOjE3MTUzNTA3MjEsImp0aSI6ImIxYjMzNGE1LWZhNDEtNDg3Ni1iMTVkLTRiZmI0NWU0ZjRlNCIsInN1YiI6MzA0Nzc5MDYwfQ.RSkmIyHUhIbarqTWY9bgQbDwfgEwSwMGB7Z-l_CjYpI3rdvnQSQ2ronfdajjKnXs36WHg5HPCooL8UOIFhiFNg'
 
+
 window.addEventListener("message", function (event) {
   const eventData = event.data
   contexto = eventData
-  console.log(contexto)
+  responsavel = eventData.data.currentAgent.name
+  cliente = eventData.data.contact.name
+  telefone = eventData.data.contact.phone_number
+  redeEscolar = eventData.data.conversation.labels[0]
+  usuario = eventData.data.contact.custom_attributes.tipo_do_usuario
+  canal = eventData.data.conversation.meta.channel
+  descricao = eventData.data.conversation.messages[0].content
+  console.log({
+    responsavel: responsavel,
+    redeEscolar: redeEscolar,
+    usuario: usuario,
+    canal: canal
+  })
 }
 )
 
-async function postContext() {
-  let results = await fetch('https://api.pipefy.com/graphql', {
+function postContext() {
+  let mutation = `
+  mutation {
+    createCard(input: {
+      pipe_id: 301394535,
+      title: "Teste leandro",
+      fields_attributes: [
+        {field_id: "respons_veis_pelo_atendimento", field_value: 304779060},
+        {field_id: "qual_o_nome_do_solicitante", field_value: ${cliente}},
+        {field_id: "telefone", field_value: ${telefone}},
+        {field_id: "rede_escolarr", field_value: "487284757"},
+        {field_id: "selecione_qual_o_seu_tipo_de_usu_rio_por_favor", field_value: ${usuario}},
+        {field_id: "tipo_de_solicita_o", field_value: "Dúvida"},
+        {field_id: "canal_de_atendimento", field_value: "Whatsapp"},
+        {field_id: "descreve_aqui_sua_solicita_o_por_favor", field_value: ${descricao}},
+      ]
+    }
+    ) {
+      card {
+        title
+      }
+    }
+  }`
+
+  let results = fetch('https://api.pipefy.com/graphql', {
     method: 'POST',
 
     headers: {
@@ -30,31 +66,11 @@ async function postContext() {
       "Authorization": `Bearer ${token}`
     },
 
-    body: JSON.stringify({
-      query: `{
-        mutation createCard(input: {
-            pipe_id: 301394535,
-            title: "Teste leandro",
-            fields_attributes: [
-              {field_id: "respons_veis_pelo_atendimento", field_value: 304779060},
-              {field_id: "qual_o_nome_do_solicitante", field_value: "Teste 123"},
-              {field_id: "telefone", field_value: "95252616"},
-              {field_id: "rede_escolarr", field_value: "487284757"},
-              {field_id: "selecione_qual_o_seu_tipo_de_usu_rio_por_favor", field_value: "Equipe administrativa"},
-              {field_id: "tipo_de_solicita_o", field_value: "Dúvida"},
-              {field_id: "canal_de_atendimento", field_value: "Whatsapp"},
-              {field_id: "descreve_aqui_sua_solicita_o_por_favor", field_value: "Whatsapp"},
-            ]
-          }
-          ) {
-            card {
-              title
-            }
-          }
-        
-        }`
-    })
+    body: JSON.stringify({ query: mutation })
   })
-  let characters = await results.json();
-  console.log(characters.data)
+  .then(res => res.json())
+  .then(console.log)
+  .catch(console.error);
+  // let characters = results.json();
+  // console.log(characters.data)
 }
